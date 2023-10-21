032394ad3660de2f29a0a9991e7a86869fb7b26a
Ozan Doğan <ozantdogan@outlook.com>
2023-10-21 11:54:23 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+#Database configuration information
+#
+DB_NAME = "YOUR_DB_NAME"
+DB_USER = "YOUR_DB_USER"
+DB_PASSWORD =  "YOUR_DB_PASSWORD"
+DB_HOST = "YOUR_DB_HOST"
+DB_PORT = "YOUR_DB_PORT"
+
+#Completed task status
+#
+COMPLETED = '🟩'
+NOT_COMPLETED = '⬛'
+
+#Commands
+#
+CLEAR = "-clr"
+ALL = "-a"
