75a98219b3fd08bede88737be760806b6917301a
Ozan Doğan <ozantdogan@outlook.com>
2023-10-22 17:14:46 +0300

    Add title

--- app_settings.py
+++ app_settings.py
@@ -12,6 +12,7 @@ ALL = "-a"
 
 colors = {
     'welcome_message_color': Fore.LIGHTMAGENTA_EX,
+    'task_manager_title_color': Fore.BLACK + Back.LIGHTMAGENTA_EX,
     'input_selection_color': Fore.LIGHTYELLOW_EX,
     'exit_program_color': Fore.WHITE,
     'input_messages_color': Fore.YELLOW,
